8173392801db5c520206030f22a7db6ac2a03c3b
Alexander <laptevx@dmail.com>
2026-01-27 12:50:25 +0300

    initial infiniti

---
+++ front-page.php
@@ -0,0 +1,127 @@
+<?php
+get_header();
+?>
+
+<section class="hero">
+    <div class="container container-hero">
+        <div class="hero-title-block">
+            <p class="hero-description">
+                <span class="color-accent">СОБСТВЕННЫЙ БОЛЬШОЙ</span> СКЛАД АВТОЗАПЧАСТЕЙ
+            </p>
+            <h1 class="hero-title">
+                Автосервис в Москве
+            </h1>
+        </div>
+        <div class="hero-info-block">
+            <div class="info-block-models">
+                <img src="<?php echo get_template_directory_uri() . '/img/lineicons_mercedes.png' ?>" alt="Мерседес">
+                <img src="<?php echo get_template_directory_uri() . '/img/simple-icons_lada.png' ?>" alt="Лада">
+                <img src="<?php echo get_template_directory_uri() . '/img/icon-park-outline_renault.png' ?>" alt="Рено">
+                <img src="<?php echo get_template_directory_uri() . '/img/lineicons_audi.png' ?>" alt="Ауди">
+                <a href="#" class="block-models-link">Все авто</a>
+            </div>
+            <p class="info-block-desc">
+                Ремонт и полное обслуживание всех марок автомобилей и особые возможности для <span class="color-accent">INFININI и NISSAN</span>
+            </p>
+            <a href="#" class="info-block-button">Записаться</a>
+        </div>
+    </div>
+</section>
+
+<section class="refit-models">
+    <div class="container-refit">
+        <div class="refit-models-item">
+            <div class="refit-card all-auto">
+                <h2 class="refit-card-title">Ремонт любых авто</h2>
+                <hr>
+                <p class="refit-card-desc">Ремонт и полное обслуживание всех марок автомобилей</p>
+                <a href="#" class="refit-card-buttom">Подробнее</a>
+            </div>
+        </div>
+        <div class="refit-models-item">
+            <div class="refit-card infiniti-auto">
+                <h2 class="refit-card-title">Ремонт Infiniti</h2>
+                <hr>
+                <p class="refit-card-desc">Ремонт и полное обслуживание <span class="color-accent">infiniti</span> с особыми возможностями</p>
+                <a href="#" class="refit-card-buttom">Подробнее</a>
+            </div>
+        </div>
+        <div class="refit-models-item three">
+            <div class="refit-card nissan-auto">
+                <h2 class="refit-card-title">Ремонт Nissan</h2>
+                <hr>
+                <p class="refit-card-desc">Ремонт и полное обслуживание <span class="color-accent">nissan</span> с особыми возможностями</p>
+                <a href="#" class="refit-card-buttom">Подробнее</a>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="market">
+    <div class="container">
+        <div class="title-market">
+            <img src="<?php echo get_template_directory_uri() . '/img/title-market.png' ?>" alt="Заголовок Инфинити Маркет">
+        </div>
+        <div class="questions-market">
+            <div class="questions-title">
+                <p><span class="color-accent">6 причин,</span> почему вам к нам? <span class="arrow"><svg width="17" height="34" viewBox="0 0 17 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M14.3891 18.0072L6.37499 26.0213L4.37183 24.0182L11.3843 17.0057L4.37183 9.99316L6.37499 7.98999L14.3891 16.0041C14.6547 16.2697 14.8039 16.63 14.8039 17.0057C14.8039 17.3813 14.6547 17.7416 14.3891 18.0072Z" fill="white" fill-opacity="0.5" />
+                        </svg>
+                    </span> </p>
+            </div>
+            <div class="questions-block">
+                <div class="questions-block-item">
+                    <div class="num color-accent">01</div>
+                    <div class="questions-block-text">
+                        <p class="questions-block-title color-accent">Один автосервис для всех ваших машин</p>
+                        <p class="questions-block-deck">Вам не нужно держать в телефоне 5 разных СТО — мы закрываем все типы работ по большинству марок.</p>
+                    </div>
+                </div>
+                <div class="questions-block-item">
+                    <div class="num color-accent">02</div>
+                    <div class="questions-block-text">
+                        <p class="questions-block-title color-accent">Собственный большой скалад запчастей</p>
+                        <p class="questions-block-deck">В 92% случаев не придется искать/ждать запчасти. На нашем складе есть всё для большинства марок и моделей автомобилей.</p>
+                    </div>
+                </div>
+                <div class="questions-block-item">
+                    <div class="num color-accent">03</div>
+                    <div class="questions-block-text">
+                        <p class="questions-block-title color-accent">Внушительный опыт мастеров</p>
+                        <p class="questions-block-deck">Наши мастера находят решения даже в самых не понятных ситуациях, за счет внушительного опыта каждого от 5 до 15 лет</p>
+                    </div>
+                </div>
+                <div class="questions-block-item">
+                    <div class="num color-accent">04</div>
+                    <div class="questions-block-text">
+                        <p class="questions-block-title color-accent">Оптимальное сочетание цены и качества</p>
+                        <p class="questions-block-deck">Приезжайте и убедитесь сами. Это касается как стоимости услуг в автосервисе, так и автозапчастей с нашего склада. </p>
+                    </div>
+                </div>
+                <div class="questions-block-item">
+                    <div class="num color-accent">05</div>
+                    <div class="questions-block-text">
+                        <p class="questions-block-title color-accent">Прозрачная коммуникация</p>
+                        <p class="questions-block-deck">Нормальный человеческий разговор вместо «такая политика сервиса».</p>
+                    </div>
+                </div>
+                <div class="questions-block-item">
+                    <div class="num color-accent">06</div>
+                    <div class="questions-block-text">
+                        <p class="questions-block-title color-accent">Гарантия и документы</p>
+                        <p class="questions-block-deck">Все работы фиксируются в заказ-наряде, есть гарантия на работы и установленные запчасти.</p>
+                    </div>
+                </div>
+                <a href="#" class="market-button">Записаться</a>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="servises">
+    <div class="container">
+        <p class="servises-title"><span class="color-accent">Наши</span> услуги</p>
+    </div>
+</section>
+
+<?php
+// get_sidebar();
+get_footer();

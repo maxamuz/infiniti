5240579d82f125bab12b13ba8d825ecb610a685d
maxamuz <laptevx@gmail.com>
2026-07-31 14:37:06 +0300

    Добавляем телефон

--- page-nissan.php
+++ page-nissan.php
@@ -773,7 +773,13 @@ get_header();
                     <path fill="#5fc2ff" d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24c1.12.37 2.33.57 3.57.57c.55 0 1 .45 1 1V20c0 .55-.45 1-1 1c-9.39 0-17-7.61-17-17c0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1c0 1.25.2 2.45.57 3.57c.11.35.03.74-.25 1.02z" />
                 </svg>
                 <p class="contact-title">Телефон</p>
-                <p class="contact-content">84991122731</p>
+                <p class="contact-content">
+                    <?php
+                    if (function_exists('theme_settings_get_phone')) {
+                        echo theme_settings_get_phone();
+                    }
+                    ?>
+                </p>
             </div>
             <div class="contacts-grid-item">
                 <!-- <img src="<?php echo get_template_directory_uri() . '/img/whatsapp-fill.png' ?>" alt="Иконка" class="contact-wt-img"> -->
